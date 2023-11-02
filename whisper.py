4d45426b46735d2c7c822e276051ec08e1c18168
Krzysztof Ruta <krzysztof.ruta@play.pl>
2023-11-02 11:02:44 +0100

    reformat transcribed text

--- whisper.py
+++ whisper.py
@@ -18,7 +18,7 @@ def transcribe(file: str) -> str:
         files = {"file": ("file1.mp3", open(file, "rb"), "application/octet-stream")}
         # Uwaga: pole data NIE jako JSON, ale tablica (k,v)
         data = [("model", "whisper-1")]
-        response = requests.request(method="post",url=openai_url, files=files, data=data, headers=header, verify=False)
+        response = requests.request(method="post", url=openai_url, files=files, data=data, headers=header, verify=False)
         result = utils.ResponseResult(response)
         transcribed = result.result["text"]
     return transcribed
@@ -33,14 +33,15 @@ def do_exercise() -> None:
     auth_token = utils.get_auth_token_or_exit(exercise)
     exercise_response = utils.get_exercise_info_or_exit(auth_token, exercise)
 
+    # extract URL from msg
     msg: str = exercise_response.result["msg"]
-    file_url: str = msg[msg.find("https://"):] # extract URL from msg
+    file_url: str = msg[msg.find("https://"):]
     print(f"got URL: {file_url}")
     filename = "whisper.mp3"
-    download_file(file_url,filename)
+    download_file(file_url, filename)
 
-    transcription = transcribe("whisper.mp3")
-    print(f"transcription: {transcription}")
+    transcription = transcribe(filename)
+    pp.pprint(f"transcription: {transcription}")
 
     utils.send_solution_or_exit(auth_token, transcription)
 
