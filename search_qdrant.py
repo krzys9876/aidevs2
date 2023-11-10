ac00869028209c03b11d0f16f55a31e781cc5b7a
Krzysztof Ruta <krzysztof.ruta@play.pl>
2023-11-10 13:45:33 +0100

    resolve search

--- search_qdrant.py
+++ search_qdrant.py
@@ -1,5 +1,4 @@
 import os.path
-
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams, PointStruct
 import sqlite3
@@ -8,8 +7,11 @@ import json
 import pprint
 import embedding
 import uuid
+from aidevslib import utils
 
 pp = pprint.PrettyPrinter(width=160)
+
+exercise = 'search'
 
 BASE_PATH = "c:\\data\\docker\\"
 SQLDB_PATH = f"{BASE_PATH}sqlite\\aidevs_search.sqlite"
@@ -18,6 +20,8 @@ JSON_PATH = f"{BASE_PATH}\\tmp\\aidevs_search.json"
 COLLECTION_NAME = "aidevs_search"
 client = QdrantClient(path=QDRANT_PATH)
 sqldb = sqlite3.connect(SQLDB_PATH)
+# 300 wpisów powinno wystarczyć do rozwiązania zadania
+MAX_ITEMS = 300
 
 
 def get_source() -> str:
@@ -34,6 +38,7 @@ def get_source() -> str:
     with open(JSON_PATH, "rt") as f:
         text = f.read()
     return text
+
 
 def create_tables_if_not_exist() -> None:
     res = sqldb.execute(f"select count(*) from sqlite_master where type='table' and name='infos'")
@@ -57,7 +62,7 @@ def insert_info_if_not_exists(rec_id: int, rec_uuid: str, title: str, url: str, 
         print(f"inserting info {rec_id}")
         ninfo = remove_parentheses(info)
         ntitle = remove_parentheses(title)
-        sqldb.execute(f"insert into infos (id, uuid, title, url, info, info_date, emb_flag) " 
+        sqldb.execute(f"insert into infos (id, uuid, title, url, info, info_date, emb_flag) "
                       f"values ({rec_id}, \"{rec_uuid}\", \"{ntitle}\", \"{url}\", \"{ninfo}\", \"{info_date}\",0)")
 
 
@@ -65,6 +70,12 @@ def get_all_non_processed_infos() -> []:
     cur = sqldb.cursor()
     cur.execute("select * from infos where emb_flag=0 order by id")
     return cur.fetchall()
+
+
+def get_info_by_id(rec_id) -> []:
+    cur = sqldb.cursor()
+    cur.execute(f"select * from infos where id={rec_id}")
+    return cur.fetchone()
 
 
 def update_info_processed(rec_id: int) -> None:
@@ -115,24 +126,48 @@ def process_one_info(info: []) -> bool:
         return False
 
 
-def do_exercise() -> None:
+def prepare_db() -> bool:
     create_tables_if_not_exist()
     create_collection_if_not_exists()
-    # 300 wpisów powinno wystarczyć do rozwiązania zadania
-    copy_source_to_db(json.loads(get_source())[:300])
-
+    copy_source_to_db(json.loads(get_source())[:MAX_ITEMS])
     counter = 0
     for i in get_all_non_processed_infos():
         print(f"processing id {i[0]}")
         pp.pprint(i)
         if process_one_info(i):
             counter += 1
-
     print(f"processed: {counter}")
-    sqldb.close()
+
+    return len(get_all_non_processed_infos()) == 0
+
+
+def do_exercise() -> None:
+    if not prepare_db():
+        # to mogłaby być pętla, ale ze względu na częste błędy zwracane przez openai API wolę wiedzieć, co się dzieje
+        print("run preparation again")
+        exit(0)
+
+    auth_token = utils.get_auth_token_or_exit(exercise)
+    exercise_response = utils.get_exercise_info_or_exit(auth_token, exercise)
+    question = exercise_response.result["question"]
+    print(f"got question: {question}")
+
+    question_emp = embedding.calc_embedding(question)
+    search_res = client.search(collection_name=COLLECTION_NAME, query_vector=question_emp, limit=1)
+    if not search_res:
+        print("FATAL not found")
+        exit(1)
+    print(search_res[0])
+    found_id = int(search_res[0].payload["id"])
+    found_rec = get_info_by_id(found_id)
+    pp.pprint(found_rec)
+    answer = found_rec[3]
+
+    utils.send_solution_or_exit(auth_token, answer)
 
 
 if __name__ == '__main__':
     print("START")
     do_exercise()
+    sqldb.close()
     print("END")
